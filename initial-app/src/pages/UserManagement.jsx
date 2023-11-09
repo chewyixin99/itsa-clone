07951fbd0451a5c53072a4d021fcdf91b0c7327b
Chew Yi Xin <chewyixin1999@gmail.com>
2023-11-09 18:40:58 +0800

    styling changes

--- initial-app/src/pages/UserManagement.jsx
+++ initial-app/src/pages/UserManagement.jsx
@@ -1,50 +1,64 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-const BE_URL = `${import.meta.env.VITE_BACKEND_URL}:${import.meta.env.VITE_BACKEND_PORT}`
-
+const BE_URL = `${import.meta.env.VITE_BACKEND_URL}:${
+  import.meta.env.VITE_BACKEND_PORT
+}`;
 
 const UserManagement = () => {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
 
-    useEffect(() => {
-        const data = JSON.parse(localStorage.getItem("user"))
-        
-        const token = data.accessToken || data.access_token
-        const config = {
-            headers: { Authorization: `Bearer ${token}` }
-        };
-        axios.get(`${BE_URL}/user/getallusers`, config)
-        .then((response) => {
-            setUsers(response.data);
-        })
-        .catch((error) => {
-            console.error("Error fetching users:", error);
-        });
-    }, []);
+  const onEditClick = () => {
+    setIsEditing(!isEditing);
+  };
 
-    return (
-        <div>
-        <h1>User Management</h1>
-        <table>
-            <thead>
-            <tr>
-                {users.length > 0 && Object.keys(users[0]).map((key) => (
-                <th key={key}>{key}</th>
-                ))}
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("user"));
+
+    const token = data.accessToken || data.access_token;
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    axios
+      .get(`${BE_URL}/user/getallusers`, config)
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []);
+
+  return (
+    <div className="h-[100vh] p-3 bg-gray-50">
+      <h1 className="pt-1 pb-6">User Management</h1>
+      <table className="bg-white">
+        <thead>
+          <tr>
+            {users.length > 0 &&
+              Object.keys(users[0]).map((key) => {
+                return (
+                  <th className="p-3 font-semibold border" key={key}>
+                    <div>{key}</div>
+                  </th>
+                );
+              })}
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              {Object.values(user).map((value, vIndex) => (
+                <td className="p-3 border" key={vIndex}>
+                  <div>{value}</div>
+                </td>
+              ))}
             </tr>
-            </thead>
-            <tbody>
-            {users.map((user, index) => (
-                <tr key={index}>
-                {Object.values(user).map((value, vIndex) => (
-                    <td key={vIndex}>{value}</td>
-                ))}
-                </tr>
-            ))}
-            </tbody>
-        </table>
-        </div>
-    );
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default UserManagement;
--- initial-app/src/pages/UserProfile.jsx
+++ initial-app/src/pages/UserProfile.jsx
@@ -4,29 +4,29 @@ import axios from "axios";
 
 const UserProfile = () => {
   const navigate = useNavigate();
-  const [userInfo, setUserInfo] = useState()
+  const [userInfo, setUserInfo] = useState();
   const [userRoles, setUserRoles] = useState([]);
-  const BE_URL = `${import.meta.env.VITE_BACKEND_URL}:${import.meta.env.VITE_BACKEND_PORT}`
+  const BE_URL = `${import.meta.env.VITE_BACKEND_URL}:${
+    import.meta.env.VITE_BACKEND_PORT
+  }`;
   useEffect(() => {
-    // alert("OTP is 123456"); 
+    // alert("OTP is 123456");
     if (!localStorage.getItem("user")) {
-      navigate("/login")
+      navigate("/login");
     }
-    const data = JSON.parse(localStorage.getItem("user"))
+    const data = JSON.parse(localStorage.getItem("user"));
     setUserRoles(data.roles); // Set userRoles
-    const token = data.accessToken || data.access_token
+    const token = data.accessToken || data.access_token;
     const config = {
-      headers: { Authorization: `Bearer ${token}` }
+      headers: { Authorization: `Bearer ${token}` },
     };
-    getUserInfo(config)
-
-
-  }, [])
+    getUserInfo(config);
+  }, []);
 
   const handleDeleteAccount = async () => {
-    if (window.confirm('Are you sure you want to delete your account?')) {
+    if (window.confirm("Are you sure you want to delete your account?")) {
       try {
-        const data = JSON.parse(localStorage.getItem('user'));
+        const data = JSON.parse(localStorage.getItem("user"));
         const config = {
           headers: { Authorization: `Bearer ${data.accessToken}` },
         };
@@ -35,13 +35,13 @@ const UserProfile = () => {
         await axios.delete(`${BE_URL}/oauth/deleteaccount`, config);
 
         // Clear user-related data from localStorage
-        localStorage.removeItem('user');
-        localStorage.removeItem('sso');
+        localStorage.removeItem("user");
+        localStorage.removeItem("sso");
 
         // Redirect the user to the login page after successful deletion
-        navigate('/login');
+        navigate("/login");
       } catch (error) {
-        console.error('Error deleting account:', error);
+        console.error("Error deleting account:", error);
         // Handle any error conditions here, e.g., show an error message to the user
       }
     }
@@ -49,54 +49,64 @@ const UserProfile = () => {
 
   const handleAuthorizeUserManagement = () => {
     if (userRoles.includes("admin") || userRoles.includes("moderator")) {
-      navigate("/user-management"); 
+      navigate("/user-management");
     }
   };
 
   const getUserInfo = async (config) => {
     let userInfoResponse;
     if (localStorage.getItem("sso")) {
-      userInfoResponse = await axios.get(`${BE_URL}/oauth/userinfo`, config)
-    }
-    else {
-      userInfoResponse = await axios.get(`${BE_URL}/user/userinfo`, config)
+      userInfoResponse = await axios.get(`${BE_URL}/oauth/userinfo`, config);
+    } else {
+      userInfoResponse = await axios.get(`${BE_URL}/user/userinfo`, config);
     }
     if (userInfoResponse.status === 200) {
-      const respData = userInfoResponse.data
+      const respData = userInfoResponse.data;
       setUserInfo({ ...respData });
     }
-  }
+  };
   return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100vh", padding: "50px" }}>
+    <div className="h-[100vh] p-3 bg-gray-50">
       {userInfo ? (
         <div>
-          <h1>User Profile</h1>
-          <table style={{ border: "2px solid black", padding: "10px", borderRadius: "10px", width: "100%", maxWidth: "800px" }}>
-            <tbody>
-              {Object.entries(userInfo).map(([key, value]) => (
-                <tr key={key}>
-                  <td>{key}:</td>
-                  <td>{value}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          {!localStorage.getItem("sso") &&
-            <button className={`p-3 mx-2 my-2 custom-button-primary margin-2`} onClick={handleDeleteAccount}>Delete Account</button>
-          }
-          {userRoles.includes("admin") || userRoles.includes("moderator") ? (
-            <button
-              style={{ backgroundColor: 'grey', color: '#fff' }} 
-              className={`p-3 mx-2 my-2 custom-button-primary margin-2`}
-              onClick={handleAuthorizeUserManagement}
-            >
-              User Management
-            </button>
-          ) : null}
+          <h1 className="pt-1 pb-6">User Profile</h1>
+          <div className="max-w-[50vw] bg-white">
+            {Object.entries(userInfo).map(([key, value]) => {
+              return (
+                <div
+                  className="flex items-center justify-between border-2"
+                  key={key}
+                >
+                  <div className="font-semibold min-w-[10vw] border-r-2 p-3">
+                    {key}
+                  </div>
+                  <div className="min-w-[40vw] p-3">{value}</div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="max-w-[50vw] flex justify-end items-center py-3">
+            {!localStorage.getItem("sso") && (
+              <button
+                className={`p-3 mx-2 my-2 custom-button-secondary`}
+                onClick={handleDeleteAccount}
+              >
+                Delete Account
+              </button>
+            )}
+            {userRoles.includes("admin") || userRoles.includes("moderator") ? (
+              <button
+                className={`p-3 mx-2 my-2 custom-button-primary`}
+                onClick={handleAuthorizeUserManagement}
+              >
+                User Management
+              </button>
+            ) : null}
+          </div>
         </div>
       ) : (
         <p>No user information available.</p>
-      )} 
+      )}
     </div>
   );
 };
